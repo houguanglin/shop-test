83f7ee8c804a3e501b1a79e88bc93bcd3060689a
houguanglin <houguanglin@sunseaaiot.com>
2022-12-21 15:50:02 +0800

    添加数据源信息

--- user/src/main/java/com/haha/user/service/impl/TestServiceImpl.java
+++ user/src/main/java/com/haha/user/service/impl/TestServiceImpl.java
@@ -1,7 +1,9 @@
 package com.haha.user.service.impl;
 
 import com.haha.user.service.TestService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TestServiceImpl implements TestService {
     @Override
     public String sayHi() {
